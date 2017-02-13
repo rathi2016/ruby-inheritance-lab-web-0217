5ddd9b04bbfca28eef4af315e4f0942098083d7c
github username <github email address>
2017-02-12 19:55:25 -0500

    done done

---
+++ lib/user.rb
@@ -0,0 +1,12 @@
+class User
+attr_accessor :first_name ,:last_name
+
+
+   def first_name=(name)
+     @first_name = name
+   end
+
+   def last_name=(las_name)
+     @last_name =las_name
+   end
+end
